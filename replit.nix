815938144091d304906886a2100b927b2e74004f
kamalvallecha <kamal.vallecha@c5i.ai>
2025-04-04 21:36:47 +0530

    Updated V1 Replit configuration files

--- replit.nix
+++ replit.nix
@@ -1,13 +1,13 @@
-{
+{ pkgs }: {
   description = "BidM Application";
   deps = [
-    "python3"
-    "nodejs"
+    pkgs.python3
+    pkgs.nodejs-20_x
     "postgresql"
     "yarn"
   ];
   env = {
-    PYTHONPATH = "${REPL_HOME}/backend:${PYTHONPATH}";
-    LD_LIBRARY_PATH = "${REPL_HOME}/backend:${LD_LIBRARY_PATH}";
+    PYTHONPATH = "./backend:$PYTHONPATH";
+    LD_LIBRARY_PATH = "./backend:$LD_LIBRARY_PATH";
   };
 }
